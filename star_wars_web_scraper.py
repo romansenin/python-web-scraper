c8750dd48439644e26c110d230a91409580692a2
Roman Senin <romansenin15@gmail.com>
2020-07-03 00:21:54 -0700

    added beautiful soup package to find all strong tags and print them out

--- star_wars_web_scraper.py
+++ star_wars_web_scraper.py
@@ -1,10 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
+# get html from site
 url = "https://www.starwars.com/news/15-star-wars-quotes-to-use-in-everyday-life"
 response = requests.get(url)
-
 headers = response.headers
 body = response.text[:2000]
 
-print(body)
+# drill down into html to grab desired info -- quotes and their "authors"
+soup = BeautifulSoup(response.text, "html.parser")
+quotes = soup.findAll("strong")
+print(quotes)
 
