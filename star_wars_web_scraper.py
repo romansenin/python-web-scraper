ec6951951f26ffbd9e8218c4be4feac7a3c91c02
Roman Senin <romansenin15@gmail.com>
2020-07-03 00:45:55 -0700

    added csv package to write results to a csv file

--- star_wars_web_scraper.py
+++ star_wars_web_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # get html from site
 url = "https://www.starwars.com/news/15-star-wars-quotes-to-use-in-everyday-life"
@@ -10,5 +11,15 @@ body = response.text[:2000]
 # drill down into html to grab desired info -- quotes and their "authors"
 soup = BeautifulSoup(response.text, "html.parser")
 quotes = soup.findAll("strong")
-print(quotes)
+
+with open("star_wars_html.csv", "w") as csv_file:
+  writer = csv.writer(csv_file)
+  column_headings = ["QUOTE"]
+  writer.writerow(column_headings)
+
+  for quote in quotes:
+    text_only_quote = quote.text.strip()
+    if len(text_only_quote):
+      writer.writerow([text_only_quote])   
+
 
